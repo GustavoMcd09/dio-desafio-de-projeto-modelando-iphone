5deef646909867b42fde9cb7a51d03f7b8c82c78
GustavoMcd09 <gustavosmacedopf@gmail.com>
2024-07-23 09:06:44 -0300

    testando as funcionalidades

--- dio-desafio-de-projeto-iphone/src/usuario/Usuario.java
+++ dio-desafio-de-projeto-iphone/src/usuario/Usuario.java
@@ -1,7 +1,19 @@
 package usuario;
 
+import celular.Iphone;
+
 public class Usuario {
     public static void main(String[] args) {
-        
+        Iphone iphone = new Iphone();
+
+        iphone.ligar("(15) 0 0000-0000");
+        iphone.iniciarCorreioVoz();
+        iphone.exibirPagina("www.google.com");
+        iphone.atualizarPagina();
+        iphone.adcionarNovaAba();
+        iphone.atender();
+        iphone.selecionarMusica("Can't Hold Us");
+        iphone.tocar();
+        iphone.pausar();
     }
 }
